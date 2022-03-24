e68c8ab7bdebfc8a15e802af55b9527dae658e19
Rakshit78 <rakshitsootwal@yahoo.com>
2022-03-24 12:06:11 +0530

    modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,71 @@
 import React from 'react';
 import './App.css';
-import {useState,useEffect} from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Screen from './Components/Scrren';
-import First from './Components/First'
-
+import First from './Components/First';
 
 function App() {
-  const [count,setcount]=useState(1);
-  const [loading,setloading]=useState(true);
-  const [list,setlist]=useState([]);
-  const [p,setp]=useState([]);
-  let arr:any=[]
-  
-  async function getdata()
-  {
-    const res=await fetch(`https://hn.algolia.com/api/v1/search_by_date?query=story&page=${count}`);
-    const data=await res.json();
-    arr=[data];
+  const [count, setcount] = useState(1);
+  const [loading, setloading] = useState(true);
+  const [list, setlist] = useState([]);
+  const [p, setp] = useState([]);
+
+  let arr: any = [];
+
+  async function getdata() {
+    const res = await fetch(
+      `https://hn.algolia.com/api/v1/search_by_date?query=story&page=${count}`
+    );
+    const data = await res.json();
+    arr = [data];
     setlist(arr[0].hits);
-    console.log(arr[0].hits)
+    console.log(arr[0].hits);
     setloading(false);
   }
-  useEffect(()=>{
+  useEffect(() => {
     getdata();
-   
-  },[count])
- 
+  }, [count]);
 
   //scroll
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth' 
+      behavior: 'smooth',
     });
   };
-  //
 
-  const handle=(event:any)=>{
+  const handle = (event: any) => {
     console.log(event.target.innerText);
     setcount(event.target.innerText);
     scrollToTop();
-  }
-  const handlejson=(auth:string)=>
-  {
-    const up=list.filter(({author})=>{
-      return author===auth;
-    })
+  };
+  const handlejson = (auth: string) => {
+    const up = list.filter(({ author }) => {
+      return author === auth;
+    });
     setp(up);
-  }
-  return (<BrowserRouter>
-     <Routes>
-       <Route path="/" element={<First handle={handle} handlejson={handlejson} list={list} loading={loading} count={count} setcount={setcount}/>}/>
-       <Route path="/Scrren" element={<Screen pass={p}/>}/>
-     </Routes>
-      
-    </BrowserRouter>
+  };
+  const navigate = useNavigate();
+  return (
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <First
+            handle={handle}
+            handlejson={handlejson}
+            list={list}
+            loading={loading}
+            count={count}
+            setcount={setcount}
+            scrollToTop={scrollToTop}
+            navigate={navigate}
+          />
+        }
+      />
+      <Route path='/Scrren' element={<Screen pass={p} />} />
+    </Routes>
   );
 }
 
--- src/test/First.test.tsx
+++ src/test/First.test.tsx
@@ -1,10 +1,54 @@
-import React from "react";
-import "@testing-library/jest-dom/extend-expect";
-//import renderer from "react-test-renderer";
-import { fireEvent, render, screen } from "@testing-library/react";
-import First from "../Components/First";
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, render, screen } from '@testing-library/react';
+import First from '../Components/First';
+const loading: boolean = true;
+const count: number = 0;
+describe('render First', () => {
+  test('render the component', () => {
+    render(
+      <First
+        handle={() => {}}
+        handlejson={() => {}}
+        list={undefined}
+        setcount={() => {}}
+        scrollToTop={() => {}}
+        navigate={() => {}}
+        count={count}
+        loading={loading}
+      />
+    );
+    // screen.debug();
+  });
+});
 
-
-
-
-   
+test('assert in title', () => {
+  render(
+    <First
+      handle={() => {}}
+      handlejson={() => {}}
+      list={undefined}
+      setcount={() => {}}
+      scrollToTop={() => {}}
+      navigate={() => {}}
+      count={count}
+      loading={loading}
+    />
+  );
+  expect(screen.getByRole('progressbar')).toBeInTheDocument();
+});
+test('heading', () => {
+  render(
+    <First
+      handle={() => {}}
+      handlejson={() => {}}
+      list={undefined}
+      setcount={() => {}}
+      scrollToTop={() => {}}
+      navigate={() => {}}
+      count={count}
+      loading={loading}
+    />
+  );
+  expect(screen.getByText('Datatable')).toBeInTheDocument();
+});
